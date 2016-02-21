b5a95f39e7ab7a534363b81561530becdac67dfd
AudiFromGoa <audifromgoa@gmail.com>
2016-02-21 16:29:22 -0500

    Some documentation with examples

--- 1_week/2e_sprintf.rb
+++ 1_week/2e_sprintf.rb
@@ -8,3 +8,22 @@ figure out the output being printed by of this Ruby code.
 puts "%05d" % 123
 
 =end
+
+puts "%05d" % 123
+
+# doctest: different examples for formatting
+# doctest: Formatting a number with leading zeros
+# >> '%05d' % 123
+# => '00123'
+# doctest: I like the i instead of d for integers, not decimals
+# >> '%05i' % 123
+# => '00123'
+# doctest: How do I format a float?
+# >> '%08.2f' % 123
+# => '00123.00'
+
+# an example of what we may want to accomplish
+# Tom : Jenkins
+# Sally : Struthers
+
+# "%someofcode :  someothercode" % [data]
