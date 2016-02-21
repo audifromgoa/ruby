918603aabcc475d517227a07f76f71a7fe72d068
AudiFromGoa <audifromgoa@gmail.com>
2016-02-21 16:28:19 -0500

    Sprintf Exercise

---
+++ 1_week/2e_sprintf.rb
@@ -0,0 +1,10 @@
+=begin
+Do: Exercise 2 - Format String
+Exercise 2
+
+Read the sprintf documentation and the % documentation in the String class and
+figure out the output being printed by of this Ruby code.
+
+puts "%05d" % 123
+
+=end
