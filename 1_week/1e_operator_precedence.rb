4b4094b49a42db1288d7b7fbb9be2ab143a63f84
AudiFromGoa <audifromgoa@gmail.com>
2016-02-21 15:27:50 -0500

    Working through the exercise
    You can use rubydoctest to ensure the things learned are still true as
    expressed in the "irb" style comments.

--- 1_week/1e_operator_precedence.rb
+++ 1_week/1e_operator_precedence.rb
@@ -13,12 +13,38 @@ Discuss your first guess and what you got when running the code.
 
 Goal: Understanding operator precedence and association.
 
-y = false
-z = true
-x = y or z
-puts x
-(x = y) or z
-puts x
-x = (y or z)
-puts x
+y = false      #  assign y to false
+z = true       #  assign z to true
+x = y or z     #  x is assigned the value of y which is false
+puts x         #  outputs the value of x which is false
+(x = y) or z   #  x is assigned to the value of y which is false
+puts x         #  outputs the value of x which is false
+x = (y or z)   #  x is assigned to the value of (thinking) true
+puts x         #  This should then output true
 =end
+
+# doctest: Our guesses
+# >> y = false
+# => false
+# >> z = true
+# => true
+# >> x = y or z
+# => true
+# >> x
+# => false
+# >> (x = y) or z
+# => true
+# >> x
+# => false
+# >> x = (y or z)
+# => true
+# >> x
+# => true
+
+
+# Somethings learned...
+# Assignment does not necessarily indicate the lines' return, in another way to
+# express this, the return value of hte expression is not always the same as
+# the assignment.
+# parenthesis just like arithmetic efects order of operations.
+# the 'or' statement has lower precedence than assignment
