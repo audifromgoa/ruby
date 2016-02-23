8b766e68ccb96a17142a8c0a8526d1c74718c42e
AudiFromGoa <audifromgoa@gmail.com>
2016-02-22 20:42:30 -0500

    Tons of learning about Kernel's format method

--- 1_week/2e_sprintf.rb
+++ 1_week/2e_sprintf.rb
@@ -9,7 +9,6 @@ puts "%05d" % 123
 
 =end
 
-puts "%05d" % 123
 
 # doctest: different examples for formatting
 # doctest: Formatting a number with leading zeros
@@ -23,7 +22,18 @@ puts "%05d" % 123
 # => '00123.00'
 
 # an example of what we may want to accomplish
-# Tom : Jenkins
+#   Tom : Jenkins
 # Sally : Struthers
+# doctest: I can use formatting codes to take strings
+# >> "%13s : %s\n%13s : %s" % ['John', 'Doe', 'Abraham', 'Jehosephite']
+# => "         John : Doe\n      Abraham : Jehosephite"
+# The %13s says to take a string, and pad spaces up to a maximum of 13 characters
+# on the left of the string
 
-# "%someofcode :  someothercode" % [data]
+if __FILE__ == $PROGRAM_NAME # Application Guard, or also known as Library Guard
+  puts "%05d" % 123
+  puts "%14.13s : %s\n%14.13s : %s" % ['John Jacob Jingle Heimer', 'Schmidt', 'Abraham', 'Jehosephite']
+end
+
+# The answer to the exercise itself is that it pads the number with 0's with a
+# total # of 5 characters, and shows this as (returns) a string
