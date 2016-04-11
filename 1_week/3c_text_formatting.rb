705d74f9a3f8cccb8d908e6ad0d1b224c42f830a
AudiFromGoa <audifromgoa@gmail.com>
2016-04-10 21:39:00 -0400

    WIP: This produces an ugly table

--- 1_week/3c_text_formatting.rb
+++ 1_week/3c_text_formatting.rb
@@ -1,38 +1,28 @@
-first_row = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-#row2 = first_row { |1| first_row.each * 2 }
-#first_row.map { |r1| 2*r1 }
-#first_row.each { |a| print a*2, " " "\n" }
-padding = 4
-puts "  Time Table  "
-puts "*...*"
-first_row.each { |a| print sprintf('%4d', a*1) }
-puts "\n"
-first_row.each { |a| print format "%#{padding}d", a*2 }
-puts "\n"
-first_row.map { |a| print a*3, " " }
-puts "\n"
-first_row.each { |a| print a*4, " " }
-puts "\n"
-first_row.each { |a| print a*5, " " }
-puts "\n"
-first_row.each { |a| print a*6, " " }
-puts "\n"
-first_row.each { |a| print a*7, " " }
-puts "\n"
-first_row.each { |a| print a*8, " " }
-puts "\n"
-first_row.each { |a| print a*9, " " }
-puts "\n"
-first_row.each { |a| print a*10, " " }
-puts "\n"
-puts "\n"
+def tablatorinatorizer(number)
+  result = ''
+  range = 1..number
+  range.each do |v|
+    range.each do |w|
+      result << "#{v*w} "
+    end
+    result << "\n"
+  end
+  result
+end
 
-p first_row
+if __FILE__ == $PROGRAM_NAME
+  # We want to be able to produce a "table" of values of numbers.
+  puts "  Time Table  "
+  puts "*...*"
+  padding = 4
+  puts tablatorinatorizer(5)
+end
+
 # until first_row[-1] do
 # first_row.each { |a| print a* first_row[0], " " }
 # first_row[+1]
 # p a
 # end
-# 
-# 
+#
+#
 # puts "\n"
