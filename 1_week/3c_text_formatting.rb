1337884d775222ea041fc770d2f9befe8ec95905
AudiFromGoa <audifromgoa@gmail.com>
2016-04-10 21:54:26 -0400

    WIP: Got columnar data nicely formatted

--- 1_week/3c_text_formatting.rb
+++ 1_week/3c_text_formatting.rb
@@ -1,19 +1,28 @@
 def tablatorinatorizer(number)
+  item_format = "%#{column_width(number)}i"
   result = ''
   range = 1..number
   range.each do |v|
     range.each do |w|
-      result << "#{v*w} "
+      result << item_format % [v * w]
     end
     result << "\n"
   end
   result
 end
 
+# doctest: column width given the upper bound of 10
+# >> column_width(10)
+# => 4
+def column_width(number_given)
+  (number_given * number_given).to_s.size + 1
+end
+
+puts tablatorinatorizer(10)
 if __FILE__ == $PROGRAM_NAME
   # We want to be able to produce a "table" of values of numbers.
   puts "  Time Table  "
-  puts "*...*"
+  puts "*...*" * 10
   padding = 4
   puts tablatorinatorizer(5)
 end
