0c1da96bd3a6b6c93dd3dd2d5c26f9a7024d513d
AudiFromGoa <audifromgoa@gmail.com>
2016-04-11 22:12:25 -0400

    Adding Challenge1 though it has some issues

--- SecondTake/challenge1.rb
+++ SecondTake/challenge1.rb
@@ -1,9 +1,14 @@
-def age_years_months(960000)
+input = [979000000,2158493738,246144023,1270166272,1025600095]
+def age_years_months(input*)
 
 seconds_in_year = 365 * 24 * 60 * 60
 seconds_in_month = 30 * 24 * 60 * 60
-age_in_years = age_in_seconds / seconds_in_year
-age_in_months = age_in_seconds % seconds_in_year
-#puts "age_in_years" age_in_m:q
-#onths 
+input.each { |a| print 979000000 / seconds_in_year.to_i}
+#age_in_months = 979000000 % 31536000.0
+year, months = a.to_s.split(".")
+final = months.to_i / 12.0
+puts "Your age is #{year} and #{final} months old"
+
+
 end
+puts age_years_months
