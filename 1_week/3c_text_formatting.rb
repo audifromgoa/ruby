26d45851fadc64fa74bd284b406823a597605d64
AudiFromGoa <audifromgoa@gmail.com>
2016-04-23 22:30:07 -0400

    Move Column exercise to SecondWeek

--- 1_week/3c_text_formatting.rb
+++ 1_week/3c_text_formatting.rb
@@ -1,39 +1,37 @@
-# doctest: Should generate a table that is columnar
-# >> tablatorinatorizer(2)
-# => " 1 2\n 2 4\n"
-# doctest: How does this handle a 0x0 grid?
-# >> tablatorinatorizer(0)
-# => " 0\n"
-# doctest: How does this handle a 0x0 grid?
-# >> tablatorinatorizer(1)
-# => " 1\n"
-def tablatorinatorizer(number)
-  item_format = "%#{column_width(number)}i"
-  result = ''
-  range = 1..number
-  range.each do |v|
-    range.each do |w|
-      result << item_format % [v * w]
+#Program to create Multiplcation table for Number provided
+#Add Title to the table
+#Decorate the table
+#All the elements must be  equally spaced
+#
+def table_gen(number)
+  range = (1..number)
+  formatted_calculation = "%#{calculated_padding(number)}i"
+  result = ""
+  range.each do |x|
+    range.each do |y|
+     result << formatted_calculation % (y * x)
     end
     result << "\n"
   end
   result
 end
 
-# doctest: column width given the upper bound of 10
-# >> column_width(10)
-# => 4
-def column_width(number_given)
-  (number_given * number_given).to_s.size + 1
+def calculated_padding(largest_multiplier)
+  (largest_multiplier ** 2).to_s.size + 1
+end
+def decorator(number)
+  '=' * ((number * calculated_padding(number)) + 1)
 end
 
-if __FILE__ == $PROGRAM_NAME
-  puts "Thank you for using the great and awe inspiring table maker!"
-  puts "Please enter the size of the table you would like to generate."
-  print "We will take a single number: "
-  size = gets.to_i
-  puts "Thank you for  your size input of #{size}.  Is this correct?"
-  gets.chomp
-  puts "Too bad, we are going to use it anyway!"
-  puts tablatorinatorizer(size)
-end
+number = 9
+puts decorator(number)
+puts table_gen(number)
+puts decorator(number)
+__END__
+
+===========
+ 1 2  3  4
+ 2 4  6  8
+ 3 6  9 12
+ 4 8 12 16
+===========
