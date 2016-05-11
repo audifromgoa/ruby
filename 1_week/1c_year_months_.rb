d84121dc2da7ec69e926988095a98c8d7c126fac
AudiFromGoa <audifromgoa@gmail.com>
2016-05-10 21:30:41 -0400

    Age is a reporting method, it returns nil

--- 1_week/1c_year_months_.rb
+++ 1_week/1c_year_months_.rb
@@ -14,11 +14,11 @@ age_in_seconds = [97900000, 2158493738, 246144023, 1270166272, 1025600095]
 
 def age(x)
 age_in_years = ""
-#age_in_years.split('.') = 2158493738.0 / (365 * 12 * 24 * 60 * 60)
-age_in_years = x / (365.0 * 12 * 24 * 60 * 60)
+age_in_years = (2158493738.0 / (365 * 12 * 24 * 60 * 60))
+#age_in_years = x / (365.0 * 12 * 24 * 60 * 60)
 
-#Puts "You are #{age_in_year.to_s.split('.').first} and #{age_in_year.to_s.split('.').second}"
-Puts "#{age_in_year.to_s.split('.').first}"
+Puts "You are #{age_in_year.to_s.split('.').first} and #{age_in_year.to_s.split('.').second}"
+#Puts "#{age_in_year.to_s.split('.').first}"
 end
-#puts age 2158493738
-puts age 120000000000000
+p age 10
+#puts age 120000000000000
