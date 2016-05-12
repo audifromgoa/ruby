2e56b0d1fd5d7a0e32e22837cb8f178aec4d5bf6
AudiFromGoa <audifromgoa@gmail.com>
2016-05-12 12:20:24 -0400

    Don't return a string

--- 1_week/3e_how_old.rb
+++ 1_week/3e_how_old.rb
@@ -1,13 +1,20 @@
-#Exercise 3
+# Exercise 3
 
-#Write a Ruby program that displays how old I am, if I am 979000000 seconds old. 
-#Display the result as a floating point (decimal) number to two decimal places (for example, 17.23).
-
-#    Note: To format the output to say 2 decimal places, we can use the Kernel's format method.
+# Write a Ruby program that displays how old I am, if I am 979000000 seconds
+# old.
 #
-#    For example, if x = 45.5678 then format("%.2f", x) will return the string 45.57
+# Display the result as a floating point (decimal) number to two decimal places
+# (for example, 17.23).
+
+#    Note: To format the output to say 2 decimal places, we can use the
+#    Kernel's format method.
+#
+#    For example, if x = 45.5678 then format("%.2f", x) will return the string
+#    45.57
 
 def age_in_year(seconds)
-  x = sprintf("%.2f", seconds.to_i / (365 * 24 * 60 * 60.0))
+  seconds.to_i / (365 * 24 * 60 * 60.0)
 end
-puts age_in_year 979000000
+
+seconds_old = 979_000_000
+puts format('%.2f', age_in_year(seconds_old) + age_in_year(100_000_000))
