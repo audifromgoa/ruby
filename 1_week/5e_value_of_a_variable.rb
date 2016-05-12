28f0976935a00aff7265a2377324db35a73ea81a
AudiFromGoa <audifromgoa@gmail.com>
2016-05-12 12:54:55 -0400

    Fix style offenses

--- 1_week/5e_value_of_a_variable.rb
+++ 1_week/5e_value_of_a_variable.rb
@@ -1,12 +1,12 @@
-#Exercise 5
+# Exercise 5
 
-#The following program prints the value of the variable. Why?
+# The following program prints the value of the variable. Why?
 
-#my_string = 'Hello Ruby World' 
+# my_string = 'Hello Ruby World'
 
-#def my_string
-#    'Hello World' 
-#end
+# def my_string
+#     'Hello World'
+# end
 #
-#puts my_string
-#end
+# puts my_string
+# end
