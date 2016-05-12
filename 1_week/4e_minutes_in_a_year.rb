16e2c1bca79184686a08eb768eff45abd4ab4be6
AudiFromGoa <audifromgoa@gmail.com>
2016-05-12 12:47:45 -0400

    As a method, satisfies the program requirement

--- 1_week/4e_minutes_in_a_year.rb
+++ 1_week/4e_minutes_in_a_year.rb
@@ -1,10 +1,12 @@
-#Exercise 4
+# Exercise 4
 
-#Write a Ruby program that tells you how many minutes there are in a year (do not bother right now about leap years etc.).
+# Write a Ruby program that tells you how many minutes there are in a year (do
+# not bother right now about leap years etc.).
 
 def minutes_in_year
-
-365 * 24 * 60
-
+  365 * 24 * 60
 end
-puts "There are #{minutes_in_year} minutes in a Year"
+
+if __FILE__ == $PROGRAM_NAME
+  puts "There are #{minutes_in_year} minutes in a Year"
+end
