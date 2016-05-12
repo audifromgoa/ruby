bd13931066be556f5fa1e273de7abad7f2932952
AudiFromGoa <audifromgoa@gmail.com>
2016-05-12 12:02:39 -0400

    Convert now returns a number, and it is not lossy.
    Wrote some tests for Convert which was returning a string

--- 1_week/6e_convert.rb
+++ 1_week/6e_convert.rb
@@ -6,7 +6,16 @@
 #  To format the output to say 2 decimal places, we can use the Kernel's format method. 
 #  For example, if x = 45.5678 then format("%.2f", x) will return the string 45.57. Another way is to use the round function as follows:
 
+# doctest: body temperature is 98.6, and should be 37.00
+# >> convert(98.6)
+# => 37.00
+# doctest: This is a body temperature as well, but should not be 37.00
+# >> convert(99).round(6)
+# => 37.222222
 def convert(f)
-  sprintf("%.2f", (f - 32) / 1.8)
+  (f.to_f - 32) * 5/9
 end
-puts convert 100
+
+if __FILE__ == $PROGRAM_NAME
+  puts sprintf("%.2f", convert(100) + convert(212))
+end
