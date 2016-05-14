6c97f806f03e3509e22cca3ba714951e9c9462a0
AudiFromGoa <audifromgoa@gmail.com>
2016-05-13 22:59:45 -0400

    Adding basic temperature conversion script

---
+++ challenge/2c_convert_basic.rb
@@ -0,0 +1,20 @@
+####Program to covert Celcius to Farenheit and Vice Versa######
+########
+
+def c_to_f(value)
+   (@value.to_f * 1.8) + 32
+end
+def f_to_c(value)
+(@value.to_f - 32 ) * 0.55
+end
+
+temperature = gets.chomp
+temperature =~ /(\d+\.?\d+)(\w)/
+@value = $1
+@unit = $2
+
+if @unit == 'c'
+  puts c_to_f @value
+else
+  puts f_to_c @value
+end
